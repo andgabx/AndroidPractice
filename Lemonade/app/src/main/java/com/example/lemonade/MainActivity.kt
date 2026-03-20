2b9a3942d5b1815d2bcf93275702f2efe5c92220
Anderson Gabriel <agvms@cesar.org.br>
2026-03-20 11:33:47 -0300

    add: landscape preview

--- Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -19,7 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -42,8 +39,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LemonadeApp() {
-    var currentStep by remember { mutableStateOf(1) }
-    var squeezeCount by remember { mutableStateOf(0) }
+    var currentStep by rememberSaveable { mutableStateOf(1) }
+    var squeezeCount by rememberSaveable { mutableStateOf(0) }
 
     Surface(
         modifier = Modifier.fillMaxSize(),
@@ -130,6 +127,14 @@ fun LemonTextAndImage(
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    MaterialTheme {
+        LemonadeApp()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 800, heightDp = 360)
+@Composable
+fun LandscapePreview() {
     MaterialTheme {
         LemonadeApp()
     }
